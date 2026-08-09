985e7ad4f0aafb06cbd526c441b4fc347ab05370
Josh Mobley <8387317+deadmonstor@users.noreply.github.com>
2026-08-09 18:51:51 +0100

    Adopt the exports codem-inventory supports

--- providers/inventory/client/codem-inventory.lua
+++ providers/inventory/client/codem-inventory.lua
@@ -27,6 +27,23 @@ local provider = {
 	GetImagePath = function()
 		return "codem-inventory/html/itemimages"
 	end,
+
+	OpenInventory = function(inventoryType, data)
+		if inventoryType == "shop" then
+			TriggerEvent("codem-inventory:openshop", data and (data.id or data.name) or data)
+			return true
+		end
+
+		TriggerServerEvent(
+			"codem-inventory:server:openstash",
+			data and (data.id or data.name) or data,
+			data and data.slots,
+			data and data.maxweight,
+			data and data.label
+		)
+
+		return true
+	end,
 }
 
 return provider
