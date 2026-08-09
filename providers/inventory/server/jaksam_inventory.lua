2e5decbdbfe69d9e1774682194c64493e415f7c3
Alivemonstor <alivemonstor55@gmail.com>
2026-08-09 18:50:47 +0100

    Add RegisterStash to jaksam_inventory provider

--- providers/inventory/server/jaksam_inventory.lua
+++ providers/inventory/server/jaksam_inventory.lua
@@ -33,6 +33,18 @@ local provider = {
 	RemoveItem = function(inv, item, count, metadata)
 		return exports[RESOURCE_INVENTORY]:removeItem(inv, item, count or 1, metadata) ~= false
 	end,
+
+	RegisterStash = function(stashId, label, slots, maxWeight, owner, groups)
+		return exports[RESOURCE_INVENTORY]:registerStash({
+			id = stashId,
+			label = label or stashId,
+			maxSlots = slots or 100,
+			maxWeight = maxWeight or 100000,
+			isPrivate = owner and true or false,
+			allowedJobs = groups,
+			runtimeOnly = false,
+		})
+	end,
 }
 
 return provider
